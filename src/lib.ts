73830adab111f20283665773bc4e280ac796e8ed
hoah2333 <c3n6fak@gmail.com>
2026-01-31 14:21:37 +0800

    1.4.7：源API已不支持all搜索，将其去除

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,10 @@
 import type { TitleQuery, UserQuery, UserRankQuery } from "./types";
 
-const apiList: string[] = ["https://api.crom.avn.sh/graphql", "https://apiv1.crom.avn.sh/graphql", "https://zh.xjo.ch/crom/graphql"];
+const apiList: string[] = [
+  "https://api.crom.avn.sh/graphql",
+  "https://apiv1.crom.avn.sh/graphql",
+  "https://zh.xjo.ch/crom/graphql",
+];
 
 export const branchInfo: Record<string, { url: string }> = {
   "cn": { url: "http://backrooms-wiki-cn.wikidot.com" },
@@ -30,7 +34,6 @@ export const branchInfo: Record<string, { url: string }> = {
   "scp-uk": { url: "http://scp-ukrainian.wikidot.com" },
   "scp-vn": { url: "http://scp-vn.wikidot.com" },
   "na": { url: "http://nationarea.wikidot.com" },
-  "all": { url: "" },
 };
 
 export async function cromApiRequest(
